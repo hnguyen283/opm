9ea299568726f9270f952765ac2e2f62c8232815
Hung Nguyen <hnguyen283@csc.com>
2016-10-14 16:40:50 +0700

    Delete target folder

--- target/opm/resources/app-code/common-ctr.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-angular.module('commonController', []).controller('CommonController', ['$scope', '$http', 
-function ($scope,$http) {
-	$http.get('load/byname/account-list').success(function(data) {			
-		$scope.accounts = angular.fromJson(data);
-	});
-}]);
